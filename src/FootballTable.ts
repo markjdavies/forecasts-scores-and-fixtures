b49dc52e4c5794768df58897389860353332aff3
markjdavies <46246921+markjdavies@users.noreply.github.com>
2023-04-22 15:58:16 +0100

    feat: add tables types

---
+++ src/FootballTable.ts
@@ -0,0 +1,23 @@
+import { FootballTournament } from './FootballTournament';
+
+export interface FootballTable {
+  tournament: FootballTournament;
+  rows: FooballTableRow[];
+}
+
+export interface FooballTableRow {
+  movement: 'U' | 'D' | '';
+  teamName: string;
+  played: number;
+  won;
+  number;
+  drawn: number;
+  lost: number;
+  for: number;
+  against: number;
+  goalDifference: number;
+  points: number;
+  form: Result[];
+}
+
+export type Result = 'W' | 'D' | 'L';
