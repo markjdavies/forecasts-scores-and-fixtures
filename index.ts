1a7cc179f18190e6cd0cdd530aec370c81c9e34d
markjdavies <46246921+markjdavies@users.noreply.github.com>
2023-04-22 16:39:03 +0100

    fix: table export stil wrong

--- index.ts
+++ index.ts
@@ -4,4 +4,4 @@ export { FootballTeam } from './src/FootballTeam';
 export { Scores } from './src/Scores';
 export { FixtureFinder, FinderFactory } from './src/FixtureFinder';
 export { FootballTournament } from './src/FootballTournament';
-export * from './src/FootballTable';
+export { FootballTable, FooballTableRow, Result } from './src/FootballTable';
