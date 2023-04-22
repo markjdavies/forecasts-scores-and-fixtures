43f703cbc62fe010e81fbfed57e0647db9f2734b
markjdavies <46246921+markjdavies@users.noreply.github.com>
2023-04-22 16:28:51 +0100

    fix: tables types not exported

--- index.ts
+++ index.ts
@@ -4,3 +4,4 @@ export { FootballTeam } from './src/FootballTeam';
 export { Scores } from './src/Scores';
 export { FixtureFinder, FinderFactory } from './src/FixtureFinder';
 export { FootballTournament } from './src/FootballTournament';
+export * from './src/FootballTable';
